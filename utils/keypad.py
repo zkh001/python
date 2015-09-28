4ebf48be4a7c458543b4f12dcdd0761d3663489b
zkh001 <hamakaz.sigia@gmail.com>
2015-09-28 18:46:54 +0900

    appen

--- utils/keypad.py
+++ utils/keypad.py
@@ -1,39 +1,65 @@
 class Keypad:
+    
     def __init__(self):
         import platform
-        self.os_type = platform.system()
+
+        if platform.system() == 'Windows':
+            self.reader = self.OnWindows()
+        else:
+            self.reader = self.OnOthers()
 
     def initialize(self):
-        if self.os_type == 'Windows':
-            import msvcrt
-        else:
-            import sys
-            import tty
-            import termios
-
-            self.term_fd = sys.stdin.fileno()
-            self.old_attr = termios.tcgetattr(self.term_fd)
-            tty.setraw(self.term_fd)
-
-    def destroy(self):
-        if self.os_type != 'Windows':
-            import termios
-            termios.tcsetattr(self.term_fd, termios.TCSADRAIN, self.old_attr)
+        self.reader.initialize()
             
     def readkey(self):
-        if self.os_type == 'Windows':
-            return msvcrt.getch()
-        else:
-            return sys.stdin.read(1)
-    
+        return self.reader.read()
+ 
     # with statement is available
     def __enter__(self):
         self.initialize()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.destroy()
+        self.reader.destroy()
         return True             # for suppressing exceptions
+
+    class OSTemplate:
+        def initialize(self):pass
+        def read(self):pass
+        def destroy(self):pass
+
+        def __caller__(self): return self.read()
+    
+
+    class OnWindows(OSTemplate):
+        def __init__(self):
+            global msvcrt
+            import msvcrt
+
+        def read(self):
+            return msvcrt.getch()
+
+    class OnOthers(OSTemplate):
+        def __init__(self):
+            global sys
+            global termios
+            global tty
+
+            import sys
+            import tty
+            import termios
+
+        def initialize(self):
+            self.term_fd = sys.stdin.fileno()
+            self.old_attr = termios.tcgetattr(self.term_fd)
+            tty.setraw(self.term_fd)
+
+        def read(self):
+            return sys.stdin.read(1)
+
+        def destroy(self):
+            termios.tcsetattr(self.term_fd, termios.TCSADRAIN, self.old_attr)
+
 
 
 if __name__ == '__main__':
@@ -41,9 +67,9 @@ if __name__ == '__main__':
     with Keypad() as k:
         while True:
             key = k.readkey()
-            print "%c" % key
+            print( "{0}\r".format(key) )
             if key == 'q':
-                print "bye"
+                print( "bye" )
                 break
-            time.sleep(0.5)
+            time.sleep(0.05)
             
